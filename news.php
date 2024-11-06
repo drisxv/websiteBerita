33cde643d3dc7c81f525ad9e0f99f2e1e19763fc
Idris <148603387+drisxv@users.noreply.github.com>
2024-11-06 13:57:21 +0700

    Add files via upload

---
+++ news.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prediksi Nominasi Game Awards 2024</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="icon" href="assets/img/logo.png" type="png">
+    <script src="assets/script/script.js"></script>
+    <style>
+        :root {
+  --base-color: #f3c623;
+  --base-variant: #f5f5f5;
+  --text-color: #000000;
+  --secondary-text: #f4f6ff;
+  --primary-color: #f0f8ff;
+  --accent-color: #eb8317;
+}
+.darkmode {
+  --base-color: #914512;
+  --base-variant: #0b192c;
+  --text-color: #ffffff;
+  --secondary-text: #ffffff;
+  --primary-color: #001f3f;
+  --accent-color: #ff6500;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+html {
+  height: 100%;
+  box-sizing: border-box;
+}
+body {
+  position: relative;
+  padding-bottom: 2rem;
+  margin-bottom: 4rem;
+  min-height: 100%;
+  background-color: var(--base-variant);
+}
+ul li {
+  list-style: none;
+}
+a {
+  color: var(--secondary-text);
+  text-decoration: none;
+}
+header {
+  background-color: var(--base-color);
+  padding: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+header > img {
+  height: 30px;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+.nav-bar li input {
+  height: 30px;
+  width: 200px;
+}
+.nav-bar li a {
+  background-color: var(--accent-color);
+  padding: 7px;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -ms-border-radius: 6px;
+  -o-border-radius: 6px;
+}
+#theme-switch svg {
+  height: 30px;
+  width: 30px;
+  padding: 5px;
+  border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  -ms-border-radius: 50%;
+  -o-border-radius: 50%;
+  background-color: var(--accent-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#theme-switch svg {
+  fill: var(--text-color);
+}
+#theme-switch svg:last-child {
+  display: none;
+}
+.darkmode #theme-switch svg:first-child {
+  display: none;
+}
+.darkmode #theme-switch svg:last-child {
+  display: block;
+}
+.button-header > img {
+  height: 30px;
+}
+.button-header,
+#theme-switch {
+  background-color: var(--accent-color);
+  border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  -ms-border-radius: 50%;
+  -o-border-radius: 50%;
+}
+.nav-bar {
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+}
+.nav-bar li {
+  margin: auto;
+}
+main {
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+h1 {
+  font-size: 40px;
+  color: var(--text-color);
+}
+#news-container{
+    display: flex;
+    flex-direction: column;
+}
+.news-card-container,
+.game-card-container {
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+}
+.news-card-container img,
+.game-card-container img{
+    width: 250px;
+    height: 250px;
+}
+.card {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  padding: 20px;
+  color: var(--text-color);
+  background-color: var(--primary-color);
+  margin: 20px;
+  border-radius: 10px;
+  border: 2px solid var(--base-color);
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  -ms-border-radius: 10px;
+  -o-border-radius: 10px;
+}
+.news {
+  flex: 8;
+}
+aside {
+  flex: 2;
+}
+footer {
+  background-color: var(--base-color);
+  color: var(--text-color);
+  text-align: center;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}.
+.team {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  margin-right: 20px;
+}
+/** Login Page */
+.login-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.login {
+  width: 400px;
+  margin: auto;
+  padding: 20px;
+  background-color: var(--base-color);
+  border-radius: 8px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+}
+.input-form-login {
+  color: var(--text-color);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  gap: 1em;
+}
+.button-submit-login button {
+  background-color: var(--accent-color);
+  color: var(--text-color);
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  -ms-border-radius: 5px;
+  -o-border-radius: 5px;
+  border: none;
+}
+.input-form-login input {
+  width: 100%;
+  height: 30px;
+  text-align: center;
+  border-radius: 10px;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  -ms-border-radius: 10px;
+  -o-border-radius: 10px;
+  border: none;
+}
+.register-form {
+  display: flex;
+  flex-direction: row;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+}
+.register-form a {
+  color: blue;
+}
+
+    </style>
+</head>
+<body>
+    <?php
+        include 'assets/script/connection.php';
+    ?>
+    <header class="head">
+        <img src="assets/img/logo.png" alt="logo" class="logo">
+        <ul class="nav-bar">
+            <li><input type="text" placeholder="Search"></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="popular.php">Popular</a></li>
+            <li>
+                <button id="theme-switch" onclick="darkmodeBTN()">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>
+                </but>
+            </li>
+        </ul>
+    </header>
+    <main>
+        <article class="news">
+            <?php
+                include 'assets/script/connection.php';
+                $id_berita = $_GET['id_berita'];
+                $data = mysqli_query($connection, "SELECT * FROM daftar_berita WHERE id_berita = '$id_berita'");
+                while($d = mysqli_fetch_array($data)){
+                    ?>
+                    <div id="news-container" class="card">
+                        <div class="news-card-container">
+                            <div>
+                                <img src="<?php echo $d['image_path'];?>">
+                            </div>
+                            <div>
+                                <h2><?php echo nl2br($d["judul_berita"]);?></h2>
+                                <p><?php echo nl2br($d["deskripsi_berita"]);?></p>
+                            </div>
+                        </div>
+                        <br>
+                        <?php
+                        $data = mysqli_query($connection, "SELECT * FROM daftar_game WHERE id_genre = '$id_berita'");
+                        while($d = mysqli_fetch_array($data)){
+                        ?>
+                        <div class="game-card-container">
+                            <div>
+                                <img src="<?php echo $d['game_image']?>" alt="">
+                            </div>
+                            <div>
+                                <h2><?php echo nl2br($d["judul_game"]); ?></h2>
+                                <p><?php echo nl2br($d["deskripsi_game"]); ?></p>
+                            </div>
+                        </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
+            <?php
+            }
+            ?>
+        </article>
+        <aside>
+            <div class="card">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque necessitatibus officia iusto debitis, vero dicta ducimus ut maiores saepe dolor a accusamus obcaecati tempore blanditiis quasi consectetur aliquam delectus nihil!</p>
+            </div>
+    </aside>
+    </main>
+    
+    <footer>
+        <p>Copyrigth © DRISXV 2024</p>
+        <div class="team">
+            <h5>Our Team</h5>
+            <ul>
+                <lI>Ahmad Fandio Fakhrudin</lI>
+                <lI>Fahmi Idris Alimuddin</lI>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
