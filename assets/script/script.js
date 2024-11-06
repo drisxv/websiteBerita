64c91401bef7fa124e6b30d019b617621a2435e7
Idris <148603387+drisxv@users.noreply.github.com>
2024-11-06 08:18:33 +0700

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,27 @@
+// Disable right-click on the whole document
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();  // Prevent the right-click context menu
+});
+
+
+function darkmodeBTN(){
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+if(darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+})
+}
