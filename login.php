012861b85ef79b8c695e28bf15932855cd2cd892
Mmanusia <mmanusia987@gmail.com>
2024-11-08 05:25:52 +0700

    Update login.php
    aku lali setcookie ne aowkowakaowk

--- login.php
+++ login.php
@@ -86,6 +86,8 @@ session_start();
 $_SESSION = [];
 session_unset();
 session_destroy();
+setcookie('login', '', time() - 3600, '/');
+setcookie('key', '', time() - 3600, '/');
 
 header("Location: login.php");
 exit;
